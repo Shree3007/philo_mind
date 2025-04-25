669d7f67256500c40be65f5b2bf29c826ee1d97b
Siddarath M <siddarathmamadapur@gmail.com>
2025-04-25 12:15:59 +0530

    pwa initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'icons/icon-192x192.png', 'icons/icon-512x512.png'],
+      manifest: {
+        name: 'PhiloMind',
+        short_name: 'PhiloMind',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1e1e1e',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // This defines `@` as an alias for `src`
+      '@': path.resolve(__dirname, 'src')
     },
   },
 });
